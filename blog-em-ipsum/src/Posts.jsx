fa703615430834e91ac2902aa58105fff2cd8bad
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-09-28 15:13:13 +0530

    first commit

---
+++ blog-em-ipsum/src/Posts.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import { fetchPosts, deletePost, updatePost } from "./api";
+// import { PostDetail } from "./PostDetail";
+import { useQuery } from "@tanstack/react-query";
+const maxPostPage = 10;
+
+export function Posts() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  // replace with useQuery
+  const {data ,isLoading,isError,error} = useQuery({
+    queryKey:["posts"],
+    queryFn:fetchPosts
+  });
+
+  if(isLoading){
+    return(
+        <>
+            Loading.....
+        </>
+    )
+  }
+  if(isError){
+    return(
+        <>
+            <h3>You are not allowed to access data</h3>
+            <p>{error.toString()}</p>
+        </>
+    )
+  }
+
+  return (
+    <>
+      <ul>
+        {data.map((post) => (
+          <li
+            key={post.id}
+            className="post-title"
+            onClick={() => setSelectedPost(post)}
+          >
+            {post.title}
+          </li>
+        ))}
+      </ul>
+      <div className="pages">
+        <button disabled onClick={() => {}}>
+          Previous page
+        </button>
+        <span>Page {currentPage + 1}</span>
+        <button disabled onClick={() => {}}>
+          Next page
+        </button>
+      </div>
+      <hr />
+      {selectedPost && <PostDetail post={selectedPost} />}
+    </>
+  );
+}
